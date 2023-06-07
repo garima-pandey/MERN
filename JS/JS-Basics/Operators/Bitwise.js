d5fa86e619d9981115a72d77229d2f3f3754c880
Garima Pandey <pgarima148@gmail.com>
2023-06-07 11:39:44 +0530

    Add files via upload

---
+++ JS/JS-Basics/Operators/Bitwise.js
@@ -0,0 +1,6 @@
+// 1 - 00000001
+// 2 - 00000010
+
+
+console.log(1 | 2); // Bitwise OR // 3
+console.log(1 & 2); // Bitwise AND // 0
--- JS/JS-Basics/Operators/LogicalWithNonBooleans.js
+++ JS/JS-Basics/Operators/LogicalWithNonBooleans.js
@@ -7,3 +7,11 @@
 // NaN
 
 // Anything that is not falsy - truthy
+
+// short circuiting
+
+let userColor = undefined;
+let defaultColor = 'blue';
+let currentColor = userColor || defaultColor;
+
+console.log(currentColor);
